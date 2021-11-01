a87bf6b909ab1402240791fd6fdc89759c7fc8a0
Sem Jörg <sem.joerg@gmx.de>
2021-11-01 21:34:50 +0100

    Added Try-Catch in ExcelWorker_DoWork

--- TabelCombiner/ExcelLogic.cs
+++ TabelCombiner/ExcelLogic.cs
@@ -24,64 +24,86 @@ namespace TabelCombiner
 
         private static void ExcelWorker_DoWork(object sender, DoWorkEventArgs e)
         {
-            IEnumerable<FileInfo> files = e.Argument as IEnumerable<FileInfo>;
-
-            Excel.Application excelApp = new Excel.Application();
-            excelApp.Visible = false;
-            Excel.Workbooks workbooks = excelApp.Workbooks;
-            Excel._Workbook mainWorkbook = workbooks.Add();
-            Excel._Worksheet mainSheet = (Excel._Worksheet)mainWorkbook.ActiveSheet;
-            int mainSheetRowCounter = 2;
-            bool copyTabelHeadder = true;
-
-            //Read And Combine Files
-            foreach (FileInfo file in files)
+            Excel.Application excelApp = null;
+            Excel.Workbooks workbooks = null;
+            Excel._Workbook mainWorkbook = null;
+            try
             {
-                Excel._Workbook newWorkbook = null;
-                try
-                {
-                    newWorkbook = workbooks.Open(file.FullName);
-                    for (int i = 0; i < newWorkbook.Sheets.Count; i++)
-                    {
-                        Excel._Worksheet newSheet = (Excel._Worksheet)newWorkbook.Sheets[i + 1];
-                        int lastRow = LastRowTotal(newSheet);
+                IEnumerable<FileInfo> files = e.Argument as IEnumerable<FileInfo>;
 
-                        if(copyTabelHeadder)
+                excelApp = new Excel.Application();
+                excelApp.Visible = false;
+                workbooks = excelApp.Workbooks;
+                mainWorkbook = workbooks.Add();
+                Excel._Worksheet mainSheet = (Excel._Worksheet)mainWorkbook.ActiveSheet;
+                int mainSheetRowCounter = 2;
+                bool copyTabelHeadder = true;
+
+                //Read And Combine Files
+                foreach (FileInfo file in files)
+                {
+                    Excel._Workbook newWorkbook = null;
+                    try
+                    {
+                        newWorkbook = workbooks.Open(file.FullName);
+                        for (int i = 0; i < newWorkbook.Sheets.Count; i++)
                         {
-                            Excel.Range headderSource = newSheet.Range["1:1"];
-                            Excel.Range headderDestination = mainSheet.Range["1:1"];
-                            headderSource.Copy(headderDestination);
-                            copyTabelHeadder = false;
-                        }
+                            Excel._Worksheet newSheet = (Excel._Worksheet)newWorkbook.Sheets[i + 1];
+                            int lastRow = LastRowTotal(newSheet);
 
-                        Excel.Range source = newSheet.Range["2:" + lastRow];
-                        Excel.Range destination = mainSheet.Range[mainSheetRowCounter + ":" + mainSheetRowCounter];
-                        mainSheetRowCounter += lastRow - 1;
-                        source.Copy(destination);
+                            if (copyTabelHeadder)
+                            {
+                                Excel.Range headderSource = newSheet.Range["1:1"];
+                                Excel.Range headderDestination = mainSheet.Range["1:1"];
+                                headderSource.Copy(headderDestination);
+                                copyTabelHeadder = false;
+                            }
+
+                            Excel.Range source = newSheet.Range["2:" + lastRow];
+                            Excel.Range destination = mainSheet.Range[mainSheetRowCounter + ":" + mainSheetRowCounter];
+                            mainSheetRowCounter += lastRow - 1;
+                            source.Copy(destination);
+                        }
                     }
-                }
-                catch (Exception ex)
-                {
-                    Log.ErrorMessage(ex.Message);
-                }
-                finally
-                {
-                    if (newWorkbook != null)
+                    catch (Exception ex)
                     {
-                        newWorkbook.Close(false);
-                        Marshal.FinalReleaseComObject(newWorkbook);
-                        newWorkbook = null;
+                        Log.ErrorMessage(ex.Message);
                     }
+                    finally
+                    {
+                        if (newWorkbook != null)
+                        {
+                            newWorkbook.Close(false);
+                            Marshal.FinalReleaseComObject(newWorkbook);
+                            newWorkbook = null;
+                        }
+                    }
+                }
+
+                excelApp.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                Log.ErrorMessage(ex.Message);
+            }
+            finally
+            {
+                if(mainWorkbook != null)
+                {
+                    Marshal.FinalReleaseComObject(mainWorkbook);
+                    mainWorkbook = null;
+                }
+                if(workbooks != null)
+                {
+                    Marshal.FinalReleaseComObject(workbooks);
+                    workbooks = null;
+                }
+                if(excelApp != null)
+                {
+                    Marshal.FinalReleaseComObject(excelApp);
+                    excelApp = null;
                 }
             }
-
-            excelApp.Visible = true;
-            Marshal.FinalReleaseComObject(mainWorkbook);
-            Marshal.FinalReleaseComObject(workbooks);
-            Marshal.FinalReleaseComObject(excelApp);
-            mainWorkbook = null;
-            workbooks = null;
-            excelApp = null;
         }
 
         public static int LastRowTotal(Excel._Worksheet wks)
